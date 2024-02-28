84d25ac36638144f1ff2958ad388fd35eb285e1e
hvppycoding <hvppycoding@gmail.com>
2024-02-28 12:59:01 +0900

    Added *.pt files to package data

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
 
+with open('README.md', 'r') as file:
+    long_description = file.read()
 
-setup()
+setup(long_description=long_description, 
+      long_description_content_type='text/markdown')
